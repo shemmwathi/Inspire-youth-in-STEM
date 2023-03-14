0772938ecf7a6800a44f538e9857e126a9e687f7
Shem Mwathi <mwathishem12@gmail.com>
2023-03-14 21:05:18 +0300

    playlist

---
+++ week-5/websites/index.js
@@ -0,0 +1,21 @@
+let age =0
+msg=""
+function add_numbers(num1,num2)
+{
+    return num1 + num2
+
+}
+console.log(add_numbers)
+add_numbers(23,45)
+document.getElementById("confirm_btn").onclick= function check_age(){
+    console.log[Button_click]
+ age=document.getElementById("age").innerHTML.valueOf
+if (age<=18){
+    msg="you are not allowed"
+    console.log["you are not allowed"]
+    document.getElementById("get in").innerHTML=msg
+
+}else
+msg="welcome to the club"
+console.log["welcome to the club"]
+document.getElementById("get_in").innerHTML=msg}
